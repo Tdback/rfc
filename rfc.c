ebb135db534990b2fc19986af435bef0dfe2a1d3
Tyler <tylerdback.etc@proton.me>
2023-09-12 22:34:52 -0400

    Began implementing cache setup.

--- rfc.c
+++ rfc.c
@@ -1,29 +1,10 @@
 /*
- *
- * Copyright © 2023 Tyler Dunneback <tylerdback@pm.me>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the “Software”), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * rfc - an IETF RFC query tool for the terminal.
  */
 
+#include <sys/stat.h>
+
 #include <ctype.h>
-#include <errno.h>
-#include <limits.h>    /* For optarg int validation */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,6 +20,8 @@ int f_format;
 
 const char *version = "version 20230912";
 
+
+char *setup_cache_path(void);
 void open_and_write(const char *);
 
 
@@ -48,19 +31,35 @@ int main(int argc, char *argv[])
 
     if (argc == 1) {
         fprintf(stderr,
-                "Usage: rfc [-V] [-c] [-l] [-r [n]] [-f format] [-s] entry\n");
+                "Usage: rfc [-V] [-c] [-l] [-r] [-f format] [-s] entry\n");
         return 1;
     }
 
 
-    const char *format = "txt";
-    int view_recent = 5;
-    /* Initialize flags */
-    // char *ep;
-    int errno;
-    // long lval;
+    char *final_cache_dir = setup_cache_path();
 
-    while ((ch = getopt(argc, argv, "Vclr:sf:")) != -1) {
+    printf("Cache directory: %s\n", final_cache_dir);
+
+    /* Create any necessary files. */
+    struct stat sb;
+
+    if (stat(final_cache_dir, &sb) == -1) {
+        printf("Directory %s does not exist: creating is now...",
+                final_cache_dir);
+
+        if (0 != mkdir(final_cache_dir, 700)) {
+            fprintf(stderr, "Error when trying to create directory %s\n",
+                    final_cache_dir);
+            exit(1);
+        }
+
+        printf("done; Directory %s successfully created\n", final_cache_dir);
+    }
+
+    /* Time to parse flags */
+    const char *format = "txt";
+
+    while ((ch = getopt(argc, argv, "Vclrsf:")) != -1) {
         switch (ch) {
         case 'V':
             fprintf(stderr, "rfc %s\n", version);
@@ -73,11 +72,6 @@ int main(int argc, char *argv[])
             break;
         case 'r':
             f_recent = 1;
-            /*
-             * Fix this, should call a function to verify optarg is a valid
-             * number. Otherwise it'll accept anything as input.
-             */
-            view_recent = atoi(optarg);
             break;
         case 's':
             f_save = 1;
@@ -102,10 +96,7 @@ int main(int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
-    /*
-     * If -c option, clean out cache. However, make sure no other options
-     * are called with it, or print an error message.
-     */
+    /* If -c option, clean out local cache. */
     if (f_clean && (f_local || f_recent || f_save || f_format || argc != 0)) {
         fprintf(stderr, "Incorrect usage of option -c\n");
         return 1;
@@ -114,10 +105,7 @@ int main(int argc, char *argv[])
     if (f_clean && !f_local && !f_recent && !f_save && !f_format && argc == 0)
         printf("Cleaning shiz up!\n");
 
-    /*
-     * If -l option, return cached files. Similar to -c no other options should
-     * be called with it or print an error message.
-     */
+    /* If -l option, return list of all locally cached files. */
     if (f_local && (f_clean || f_recent || f_save || f_format || argc != 0)) {
         fprintf(stderr, "Incorrect usage of option -l\n");
         return 1;
@@ -127,17 +115,19 @@ int main(int argc, char *argv[])
         printf("Viewing local files\n");
     }
 
-    /*
-     * If -r option, return [n] most recent searches. Print an error if called
-     * with any other options.
-     */
+    /* If -r option, return list of all recently queried entries. */
     if (f_recent && (f_local || f_clean || f_save || f_format || argc != 0)) {
         fprintf(stderr, "Incorrect usage of option -r\n");
         return 1;
     }
 
     if (f_recent && !f_local && !f_clean && !f_save && !f_format && argc == 0) {
-        printf("Viewing recent %d files!\n", view_recent);
+        /*
+         * These files should be stored in a simple txt file within the RFC_PATH
+         * directory, and it should only maintain a list of the last 5-10 files.
+         */
+        printf("Viewing recently queried files!\n");
+        return 0;
     }
 
     /*
@@ -152,7 +142,6 @@ int main(int argc, char *argv[])
     /* Time to do some fun string concatenation in C :) */
     char *entry_to_curl = calloc(strlen(*argv) + strlen(format) + 2,
                                  sizeof(char));
-
     if (entry_to_curl == NULL) {
         fprintf(stderr, "calloc failed\n");
         return 1;
@@ -172,11 +161,45 @@ int main(int argc, char *argv[])
         // save_entry_local(*argv);
     }
 
+    /* Free all the calloc'd strings */
+    free(final_cache_dir);
     free(entry_to_curl);
 
     return 0;
 }
 
+char *setup_cache_path()
+{
+
+    /* Create a cache directory in the user's home directory */
+    const char *home_dir = getenv("HOME");
+    if (home_dir == NULL) {
+        perror("getenv");
+        exit(1);
+    }
+
+    const char *cache_dir = getenv("RFC_PATH");
+    if (cache_dir == NULL) {
+        fprintf(stderr, "RFC_PATH is not set! Defaulting to \"%s/.cache/\".\n",
+                home_dir);
+        cache_dir = ".cache/";
+    }
+
+    /* Need to add 2 here for both the null byte and the "/" */
+    char *final_cache_dir = calloc(strlen(home_dir) + strlen(cache_dir) + 2,
+                                   sizeof(char));
+    if (final_cache_dir == NULL) {
+        fprintf(stderr, "calloc failed\n");
+        exit(1);
+    }
+
+    /* Format the string holding the cache directory location */
+    strcat(final_cache_dir, home_dir);
+    strcat(final_cache_dir, "/");
+    strcat(final_cache_dir, cache_dir);
+
+    return final_cache_dir;
+}
 
 /* Call this if using curl to save the file locally */
 void write_callback(void *contents, size_t size, size_t nitems, FILE *file)
@@ -185,23 +208,6 @@ void write_callback(void *contents, size_t size, size_t nitems, FILE *file)
     fwrite(contents, size, nitems, file);
 }
 
-/*
- * Create function to validate that the input is a number
- * Need to understand this better
-
- * errno = 0;
- * lval = strtol(optarg, &ep, 10);
- * if (optarg[0] == '\0' || *ep != '\0') {
- *     fprintf(stderr, "Not a number!\n");
- *     return 1;
- * }
- * if ((errno == ERANGE && (lval == LONG_MAX || lval == LONG_MIN)) ||
- *         (lval > INT_MAX || lval < INT_MIN)) {
- *     fprintf(stderr, "Out of range!\n");
- *     return 1;
- * }
- * view_recent = lval;
- */
 
 /*
  * Will have to write using fopen contents of curl request to filenames,
